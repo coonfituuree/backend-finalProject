b2f08d89398de92283b90fc9e02853cdd2905326
Galym <galymbartai@gmail.com>
2026-02-08 02:25:14 +0500

    q

--- backend/app/config/nodemailer.js
+++ backend/app/config/nodemailer.js
@@ -4,12 +4,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASSWORD,
-    },
-})
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASSWORD,
+  },
+});
 
 export default transporter;
